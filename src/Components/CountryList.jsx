11615d1ed4b18fa18679390c297fc5958b99fc2c
Laura <97021990+lauraMeiz@users.noreply.github.com>
2022-05-02 11:36:40 +0300

    smaller

--- src/Components/CountryList.jsx
+++ src/Components/CountryList.jsx
@@ -4,18 +4,6 @@ import Country from "./Country";
 function CountryList() {
   const [countries, setCountries] = useState([]);
 
-  // const [smaller, setSmaller] = useState("");
-  // const [ocean, setOcean] = useState("");
-  // const [filterH, setFilterH] = useState("");
-  // const handleChange = (e) => {
-  //   setSearch(e.target.value);
-  // };
-  // console.log(filmai.id);
-
-  // const show = (filmas) => {
-  //   setChoice(filmas);
-  //   setFilmai("");
-  // };
   useEffect(() => {
     fetch("https://restcountries.com/v2/all?fields=name,region,area")
       .then((res) => res.json())
@@ -33,35 +21,28 @@ function CountryList() {
     setCountries(countriesCopy);
   };
 
-  // const handleInput = (e, d) => {
-  //   switch (d) {
-  //     case "EAN":
-  //       setSmaller(e.target.value);
-  //       break;
-  //     case "name":
-  //       setOcean(e.target.value);
-  //       break;
+  const sortArea2 = () => {
+    const countriesCopy = [...countries];
+    countriesCopy.sort((a, b) => b.area - a.area);
 
-  //     default:
-  //   }
-  // };
+    setCountries(countriesCopy);
+  };
+
+  const getSmaller = () => {
+    const countriesCopy = [...countries];
+
+    const filtered = countriesCopy.filter((a) => a.area < 65300);
+    setCountries(filtered);
+    console.log(filtered);
+  };
 
   return (
     <>
-      {/* <div className="buttons">
-        <button onClick={sortArea}>Acending</button>
-        <button>Descending</button>
-        <select value={filterH} onChange={(e) => handleInput(e, "smaller")}>
-          <option value="smaller">
-            That are smaller than Lithuania by area.
-          </option>
-          <option value="ocean">That are in “Oceania” region.</option>
-        </select>
-      </div> */}
       <div className="buttons">
         <button onClick={sortArea}>Acending</button>
-        <button>Descending</button>
+        <button onClick={sortArea2}>Descending</button>
       </div>
+      <button onClick={getSmaller}>Smaller than Lithuania</button>
       <Country className="one" countries={countries}></Country>
     </>
   );
